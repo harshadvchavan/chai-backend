1acdb22f6bc5d1f98b664f3568da256342a1d315
harshad <chavanharshad536@gmail.com>
2025-02-19 17:32:12 +0530

    utils code discusion

--- src/index.js
+++ src/index.js
@@ -33,3 +33,12 @@ const app = express()
 */
 
 connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(` Server is running at port : $
+            {process.env.PORT}`);
+    })
+})
+.catch( (err) => {
+    console.log("MONGO db connection is failed !!! ", err);
+})
