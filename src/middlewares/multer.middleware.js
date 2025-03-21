eb8a12c0460baa9fd265da8829fe4cfee4c83fa8
harshad <chavanharshad536@gmail.com>
2025-03-21 14:54:19 +0530

    registeruser

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,4 +1,4 @@
-import multer from "multer"
+import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -8,7 +8,6 @@ const storage = multer.diskStorage({
       cb(null, file.originalname)
     }
   })
-  
-export const upload = multer({
-     storage,
+export const upload = multer({ 
+    storage, 
 })
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,26 +1,33 @@
-import { v2 as cloudinary } from 'cloudinary'
-import fs from "fs"  // file system (used for file operations)
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
 
 
 cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
-  const uploadOnCloudinary = async (localFilePath) => {
+const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if(!localFilePath) return null
+        if (!localFilePath) return null
+        //upload the file on cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
-        console.log("file is uploaded on cloudinary", response.url);
-        return response;
-    } catch (error) {
+        //file has been uploaded successfull
+        console.log("file is uploaded on cloudinary ", response.url);
+        // unlink se public/me files nahi store hogi
         fs.unlinkSync(localFilePath)
-        return null
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        console.log("i reached here")
+        return null;
     }
-  }
+}
 
 
-  export {uploadOnCloudinary}
+
+export {uploadOnCloudinary}
